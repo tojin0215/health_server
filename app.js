c4af81ee52d01841323910043cc9ff27872a7ecf
kkyubrother@naver.com <Lun@ria1024E420>
2021-10-28 12:11:35 +0900

    Fix Server

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 
-var express = require('express');
-const session = require('express-session')
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-
-var app = express();
-
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+const express = require('express');
+const session = require('express-session');
+const app = express();
 
 app.use(session({ 
   secret: 'kwonsoryeong1113',
   resave: false, 
   saveUninitialized: true,
 }));
-const routers = require('./api')
-var sequelize = require('./models').sequelize; // sequelize require
+
+const port = 3002
+
+const routers = require('./api');
+const sequelize = require('./models').sequelize; // sequelize require
 sequelize.sync();
+
+
+var cors = require('cors')
+var bodyParser = require('body-parser')
+
+app.use(cors({
+  origin: true,
+  credentials: true
+}))
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+//POST body 등을 편리하게 추출하기 위함
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -40,4 +57,13 @@ app.use(routers.manager);
 app.use(routers.inbody);
 app.use(routers.mobile);
 
-module.exports = app;
+app.listen(port, () => console.log(`API Server listening on port ${port}`))
+
+
+process.on('uncaughtException', (err) => {
+    console.error("Server uncaughtException : Catch");
+    console.error(err);
+    process.exit(1);
+});
+
+// module.exports = app;
