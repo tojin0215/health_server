8a5f94e82648d30386422f1403fbd06b6128c6cd
xcv <xcv4716@naver.com>
2021-11-21 18:36:59 +0900

    asd

--- api/assignexercise.js
+++ api/assignexercise.js
@@ -49,7 +49,7 @@ router.route('/assignexercise')
             clue.where = {
                 fitness_no: fitness_no,
                 member_no: member_no,
-                createdAt: { [Op.like]: "%" + createdAt + "%" }
+                createdAt: { [Op.like]: "%" + req.query.createdAt + "%" }
             }
         }
         else { }
