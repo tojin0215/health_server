a399153053225bae28fa42226f94f83325232133
xcv <xcv4716@naver.com>
2021-12-27 16:48:51 +0900

    adsasd

--- models/reservation.js
+++ models/reservation.js
@@ -1,8 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
   return sequelize.define('reservation', {
     res_no: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    date: { type: DataTypes.date, allowNull: false },
-    time: { type: DataTypes.time, allowNull: false },
+    date: { type: DataTypes.DATE, allowNull: false },
+    time: { type: DataTypes.INTEGER, allowNull: false },
     exercise_name: { type: DataTypes.STRING(128), allowNull: false },
     customer_name: { type: DataTypes.STRING(64), allowNull: false },
     customer_id: { type: DataTypes.STRING(64), allowNull: true },
