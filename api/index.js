ac41e22a679013cb80fe99e12b8dee22f5e13335
xcv <xcv4716@naver.com>
2021-12-27 18:01:57 +0900

    reservation

--- api/index.js
+++ api/index.js
@@ -12,17 +12,18 @@ const alerts = require('./alerts');
 
 //api 파일 생성할 때 마다 index에도 함께 추가
 let routers = {
-    customer:customer,
-    sales:sales,
+    customer: customer,
+    sales: sales,
     customerenter: customerenter,
-    exercise:exercise,
-    assignexercise:assignexercise,
-    exerciselink:exerciselink,
-    exercisepack:exercisepack,
-    manager:manager,
-    inbody:inbody,
-    mobile:mobile,
-    alerts:alerts,
+    exercise: exercise,
+    assignexercise: assignexercise,
+    exerciselink: exerciselink,
+    exercisepack: exercisepack,
+    manager: manager,
+    inbody: inbody,
+    mobile: mobile,
+    alerts: alerts,
+    reservation: reservation,
 }
 
 module.exports = routers;
