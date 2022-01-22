44244782adb4290eb78881f76ff50d76c5d7a9a2
xcv <xcv4716@naver.com>
2022-01-22 12:30:40 +0900

    time

--- models/reservation.js
+++ models/reservation.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     isCancel: { type: DataTypes.STRING(256), allowNull: true },
     cancelComment: { type: DataTypes.STRING(256) },
     number_of_peopleFountain: { type: DataTypes.STRING(45), allowNull: true },
+    time: { type: DataTypes.INTEGER, allowNull: false }
   }, {
     timestamps: false
   });
