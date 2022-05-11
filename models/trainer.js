b898b1ba1f07ad5aa71fe655ed94c5a642a7960c
developerTojin <mb8281@gmail.com>
2022-05-11 10:42:06 +0900

    asdasd

--- models/trainer.js
+++ models/trainer.js
@@ -1,10 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define({
+  return sequelize.define("trainer", {
     idx: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     phone: { type: DataTypes.STRING(200), allowNull: false },
-
     birth: { type: DataTypes.STRING(200), allowNull: false },
-
     fitness_no: { type: DataTypes.INTEGER, allowNull: false },
     fitness_name: { type: DataTypes.STRING(20), allowNull: false },
     trainer_name: { type: DataTypes.STRING(20), allowNull: false },
