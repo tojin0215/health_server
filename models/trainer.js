3556f4545f08e6a40611721839311ab68695d95c
developerTojin <mb8281@gmail.com>
2022-05-11 10:09:19 +0900

    asdasdasd

--- models/trainer.js
+++ models/trainer.js
@@ -2,9 +2,9 @@ module.exports = (sequelize, DataTypes) => {
   return sequelize.define({
     idx: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     phone: { type: DataTypes.STRING(200), allowNull: false },
-    //아이디가 phone
+
     birth: { type: DataTypes.STRING(200), allowNull: false },
-    //비밀번호가 birth
+
     fitness_no: { type: DataTypes.INTEGER, allowNull: false },
     fitness_name: { type: DataTypes.STRING(20), allowNull: false },
     trainer_name: { type: DataTypes.STRING(20), allowNull: false },
@@ -13,3 +13,5 @@ module.exports = (sequelize, DataTypes) => {
     sex: { type: DataTypes.BOOLEAN },
   });
 };
+//아이디가 phone
+//비밀번호가 birth
