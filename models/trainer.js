347da6ec92ef45e9ced3ba4250ea242279b29459
developerTojin <mb8281@gmail.com>
2022-05-16 10:01:31 +0900

    fitness_no

--- models/trainer.js
+++ models/trainer.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
       phone: { type: DataTypes.STRING(40), allowNull: false },
       birth: { type: DataTypes.STRING(40), allowNull: false },
       trainer_name: { type: DataTypes.STRING(400), allowNull: false },
+      fitness_no: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+      },
       ment: { type: DataTypes.STRING(400), allowNull: true },
       history: { type: DataTypes.STRING(400), allowNull: true },
       sex: { type: DataTypes.INTEGER, allowNull: true },
