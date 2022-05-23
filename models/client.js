05171552512a9e4c9197f097be2c201906a26c88
developerTojin <mb8281@gmail.com>
2022-05-23 17:57:33 +0900

    address

--- models/client.js
+++ models/client.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
       birth: { type: DataTypes.STRING(40), allowNull: false },
       sex: { type: DataTypes.INTEGER, allowNull: false },
       join_route: { type: DataTypes.STRING(30), allowNull: false },
-      adress: { type: DataTypes.STRING(300), allowNull: false },
+      address: { type: DataTypes.STRING(300), allowNull: false },
       start_date: {
         type: DataTypes.DATE,
         allowNull: false,
