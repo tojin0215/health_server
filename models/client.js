afa9410d296155ebb6e1670eb37b4485d03e72b6
developerTojin <mb8281@gmail.com>
2022-05-23 13:25:38 +0900

    timestamps

--- models/client.js
+++ models/client.js
@@ -1,23 +1,29 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('client', {
-    idc: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+  return sequelize.define(
+    'client',
+    {
+      idc: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      fitness_no: { type: DataTypes.INTEGER, allowNull: false },
+      fitness_name: { type: DataTypes.STRING(20), allowNull: false },
+      client_name: { type: DataTypes.STRING(20), allowNull: false },
+      phone: { type: DataTypes.STRING(40), allowNull: false },
+      birth: { type: DataTypes.STRING(40), allowNull: false },
+      sex: { type: DataTypes.INTEGER, allowNull: false },
+      join_route: { type: DataTypes.STRING(30), allowNull: false },
+      adress: { type: DataTypes.STRING(300), allowNull: false },
+      start_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.fn('NOW'),
+      },
     },
-    fitness_no: { type: DataTypes.INTEGER, allowNull: false },
-    fitness_name: { type: DataTypes.STRING(20), allowNull: false },
-    client_name: { type: DataTypes.STRING(20), allowNull: false },
-    phone: { type: DataTypes.STRING(40), allowNull: false },
-    birth: { type: DataTypes.STRING(40), allowNull: false },
-    sex: { type: DataTypes.INTEGER, allowNull: false },
-    join_route: { type: DataTypes.STRING(30), allowNull: false },
-    adress: { type: DataTypes.STRING(300), allowNull: false },
-    start_date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: sequelize.fn('NOW'),
-    },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
--- models/customer.js
+++ models/customer.js
@@ -1,21 +1,37 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('customer', {
-        member_no :{ type: DataTypes.INTEGER, primaryKey:true, autoIncrement:true},
-        fitness_no: { type: DataTypes.INTEGER, allowNull: false },
-        name : {type:DataTypes.STRING(10), allowNull:false},
-        sex:{type:DataTypes.BOOLEAN },
-        start_date : {type:DataTypes.DATE, allowNull:false, defaultValue: sequelize.fn("NOW")},
-        period : {type:DataTypes.INTEGER, allowNull:false, defaultValue: 0},
-        phone : {type:DataTypes.STRING(32), allowNull:false},
-        solar_or_lunar : {type:DataTypes.BOOLEAN },
-        address :{type:DataTypes.STRING(100)},
-        join_route : {type:DataTypes.STRING(30) },
-        //uncollected :{ type:DataTypes.INTEGER },
-        in_charge :{type:DataTypes.STRING(10)},
-        note :{type:DataTypes.STRING(100)},
-        resi_no :{type:DataTypes.STRING(30)},
-        remained_membership :{type:DataTypes.INTEGER, allowNull: true, defaultValue: 0},
-    },{
-      timestamps:false
-    });
-}
+  return sequelize.define(
+    'customer',
+    {
+      member_no: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      fitness_no: { type: DataTypes.INTEGER, allowNull: false },
+      name: { type: DataTypes.STRING(10), allowNull: false },
+      sex: { type: DataTypes.BOOLEAN },
+      start_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.fn('NOW'),
+      },
+      period: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+      phone: { type: DataTypes.STRING(32), allowNull: false },
+      solar_or_lunar: { type: DataTypes.BOOLEAN },
+      address: { type: DataTypes.STRING(100) },
+      join_route: { type: DataTypes.STRING(30) },
+      //uncollected :{ type:DataTypes.INTEGER },
+      in_charge: { type: DataTypes.STRING(10) },
+      note: { type: DataTypes.STRING(100) },
+      resi_no: { type: DataTypes.STRING(30) },
+      remained_membership: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
