fdd3257c7be0c858c9573e5378397dfb2d992022
developerTojin <mb8281@gmail.com>
2022-05-30 14:16:28 +0900

    asd

--- api/introduce.js
+++ api/introduce.js
@@ -7,43 +7,48 @@ const Op = sequelize.Op;
 
 router
   .route('/introduce')
-  .get(function (req, res) {
-    Introduce.findAll({
-      where: { fitness_no: req.query.fitness_no },
-    });
-  })
-  .then((result) => {
-    res.json(result);
-  })
-  .catch((err) => {
-    console.error(err);
-  })
+  .get(
+    function (req, res) {
+      Introduce.findAll({
+        where: { fitness_no: req.query.fitness_no },
+      });
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+    }     
+  )
+
   .post(function (req, res) {
     Introduce.create({
       fitness_no: req.body.fitness_no,
       manager_name: req.body.manager_name,
       picture: req.body.picture,
       story: req.body.story,
-    });
+    })
+    .then(() => {
+        res.send({ success: 'insert success!' });
+      })
+      .catch((err) => {
+        console.error(err);
+      })
   })
-  .then(() => {
-    res.send({ success: 'insert success!' });
-  })
-  .catch((err) => {
-    console.error(err);
-  })
+  
   //   .put(function (req, res) {})
   .delete(function (req, res) {
     Introduce.destroy({
       where: {
         idi: req.query.idi,
       },
-    });
+    })
+    .then(() => {
+        res.send({ success: 'delete success!' });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   })
-  .then(() => {
-    res.send({ success: 'delete success!' });
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+  
 module.exports = router;
