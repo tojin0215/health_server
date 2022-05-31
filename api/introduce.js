7a1461e8ba3e18ab26263b79e00b55f95534f42e
developerTojin <mb8281@gmail.com>
2022-05-31 10:12:50 +0900

    sdfg

--- api/introduce.js
+++ api/introduce.js
@@ -26,7 +26,7 @@ router
     Introduce.create({
       fitness_no: req.body.fitness_no,
       manager_name: req.body.manager_name,
-      picture: req.body.filename,
+      picture: req.file.picture,
       story: req.body.story,
     })
       .then(() => {
