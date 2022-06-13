5a1cfa04df5d6e77cd981770b23fcb671e6f7d1a
developerTojin <mb8281@gmail.com>
2022-06-13 17:11:58 +0900

    sadasd

--- api/workout.js
+++ api/workout.js
@@ -3,3 +3,7 @@ let router = express.Router();
 var Workout = require('../models').Workout;
 const sequelize = require('sequelize');
 const Op = sequelize.Op;
+
+router.route('/workout');
+
+module.exports = router;
--- api/workoutAlloted.js
+++ api/workoutAlloted.js
@@ -3,3 +3,7 @@ let router = express.Router();
 var WorkoutAlloted = require('../models').WorkoutAlloted;
 const sequelize = require('sequelize');
 const Op = sequelize.Op;
+
+router.route('/workoutAlloted');
+
+module.exports = router;
