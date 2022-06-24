d8e19762387da0f87eea80ca5d672870aab4437c
developerTojin <mb8281@gmail.com>
2022-06-24 10:19:38 +0900

    sale update

--- api/sales.js
+++ api/sales.js
@@ -2,159 +2,138 @@ let express = require('express');
 let router = express.Router();
 var Sales = require('../models').Sales;
 
-const sequelize = require("sequelize");
+const sequelize = require('sequelize');
 const Op = sequelize.Op;
 
 require('moment-timezone');
 var moment = require('moment');
-moment.tz.setDefault("Asia/Seoul");
+moment.tz.setDefault('Asia/Seoul');
 
-router.route('/sales')
-    .get(function (req, res) {
-        // 불러오기
-        let type = req.query.type;
-        if (type === "all") { // 전체 리스트
-            //console.log(moment().format("YYYY-MM-DD"))
-            //console.log(moment().add(1,'day').format("YYYY-MM-DD"))
-            Sales.findAll({
-                where: {
-                    fitness_no: req.query.fn,
-                    //paymentDate : moment().format("YYYY-MM-DD")
-                    paymentDate: {
-                        [Op.between]: [moment().format("YYYY-MM-DD"), moment().add(1, 'day').format("YYYY-MM-DD")]
-                    }
-                }
-            })
-                .then((sales) => {
-                    res.json(sales);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    next(err);
-                });
-        } else if (type === "select") { // 원하는 날짜
-            //console.log(moment(req.query.startDate).subtract(7, 'hours').format("YYYY-MM-DD"))
-            Sales.findAll({
-                where: {
-                    fitness_no: req.query.fn,
-                    //paymentDate : moment().format("YYYY-MM-DD")
-                    paymentDate: {
-                        [Op.between]: [moment(req.query.startDate).subtract(9, 'hours'), moment(req.query.endDate).subtract(9, 'hours')]
-                    }
-                }
-            })
-                .then((sales) => {
-                    res.json(sales);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    next(err);
-                });
-        } else if (type === 'tools') { //결제도구
-            Sales.findAll({
-                where: {
-                    fitness_no: req.query.fn,
-                    paymentTools: req.query.paymentTools,
-                    paymentDate: {
-                        [Op.between]: [moment(req.query.startDate).subtract(9, 'hours'), moment(req.query.endDate).subtract(9, 'hours')]
-                    }
-                }
-            })
-                .then((sales) => {
-                    res.json(sales);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    next(err);
-                });
-        } else if (type === 'exercise') { //운동
-            Sales.findAll({
-                where: {
-                    fitness_no: req.query.fn,
-                    exerciseName: {
-                        [Op.like]: req.query.exerciseName + "%"
-                    },
-                    paymentDate: {
-                        [Op.between]: [moment(req.query.startDate).subtract(9, 'hours'), moment(req.query.endDate).subtract(9, 'hours')]
-                    }
-                }
-            })
-                .then((sales) => {
-                    res.json(sales);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    next(err);
-                });
-        } else if (type === 'customer') { //고객별
-            Sales.findAll({
-                where: {
-                    fitness_no: req.query.fn,
-                    member_no: req.query.member_no
-                }
-            })
-                .then((sales) => {
-                    res.json(sales);
-                })
-                .catch((err) => {
-                    console.error(err);
-                    next(err);
-                });
-        }
-    })
-    .post(function (req, res) {
-        // 쓰기
-        Sales.create({
-            fitness_no: req.body.fitness_no,
-            member_no: req.body.member_no,
-            exerciseName: req.body.exerciseName,
-            exercisePrice: req.body.exercisePrice,
-            //locker:req.body.locker,
-            lockerPrice: req.body.lockerPrice,
-            //sportswear:req.body.sportswear,
-            sportswearPrice: req.body.sportswearPrice,
-            paymentTools: req.body.paymentTools,
-            paymentDate: req.body.paymentDate,
-            paidMembership: req.body.paidMembership,
-            salesStart_date: req.body.salesStart_date,
-            salesDays: req.body.salesDays
-        }).then(() => {
-            res.send({ 'success': 'sales update!' });
-        })
-            .catch((err) => {
-                console.error(err);
-            });
-    })
-    .put(function (req, res) {
-        // 수정
-        // Sales.update({ 
-        //     exerciseName:  req.body.exerciseName, 
-        //     exercisePrice: req.body.exercisePrice,
-        //     //locker:req.body.locker,
-        //     lockerPrice: req.body.lockerPrice,
-        //     //sportswear:req.body.sportswear,
-        //     sportswearPrice: req.body.sportswearPrice,
-        //     paymentTools: req.body.paymentTools,
-        //     paymentDate: req.body.paymentDate,
-        //  }, { where: { member_no: req.body.member_no, fitness_no:req.query.fn } })
-        // .then((result) => {
-        // res.send('Update the diary');
-        // })
-        // .catch((err) => {
-        // console.error(err);
-        // next(err);
-        // });
-    })
-    .delete(function (req, res) {
-        //삭제
-        /*Sales.destroy({ where: { writer: "권소령", year:2021, month:1, date:14 } })
-        .then((result) => {
-        res.send('Delete the diary');
+router
+  .route('/sales')
+  .get(function (req, res) {
+    // 불러오기
+    let type = req.query.type;
+    if (type === 'all') {
+      // 전체 리스트
+      //console.log(moment().format("YYYY-MM-DD"))
+      //console.log(moment().add(1,'day').format("YYYY-MM-DD"))
+      Sales.findAll({
+        where: {
+          fitness_no: req.query.fn,
+          //paymentDate : moment().format("YYYY-MM-DD")
+          paymentDate: {
+            [Op.between]: [
+              moment().format('YYYY-MM-DD'),
+              moment().add(1, 'day').format('YYYY-MM-DD'),
+            ],
+          },
+        },
+      })
+        .then((sales) => {
+          res.json(sales);
         })
         .catch((err) => {
+          console.error(err);
+          next(err);
+        });
+    } else if (type === 'select') {
+      // 원하는 날짜
+      //console.log(moment(req.query.startDate).subtract(7, 'hours').format("YYYY-MM-DD"))
+      Sales.findAll({
+        where: {
+          fitness_no: req.query.fn,
+          //paymentDate : moment().format("YYYY-MM-DD")
+          paymentDate: {
+            [Op.between]: [
+              moment(req.query.startDate).subtract(9, 'hours'),
+              moment(req.query.endDate).subtract(9, 'hours'),
+            ],
+          },
+        },
+      })
+        .then((sales) => {
+          res.json(sales);
+        })
+        .catch((err) => {
+          console.error(err);
+          next(err);
+        });
+    } else if (type === 'tools') {
+      //결제도구
+      Sales.findAll({
+        where: {
+          fitness_no: req.query.fn,
+          paymentTools: req.query.paymentTools,
+          paymentDate: {
+            [Op.between]: [
+              moment(req.query.startDate).subtract(9, 'hours'),
+              moment(req.query.endDate).subtract(9, 'hours'),
+            ],
+          },
+        },
+      })
+        .then((sales) => {
+          res.json(sales);
+        })
+        .catch((err) => {
+          console.error(err);
+          next(err);
+        });
+    } else if (type === 'exercise') {
+      //운동
+      Sales.findAll({
+        where: {
+          fitness_no: req.query.fn,
+          exerciseName: {
+            [Op.like]: req.query.exerciseName + '%',
+          },
+          paymentDate: {
+            [Op.between]: [
+              moment(req.query.startDate).subtract(9, 'hours'),
+              moment(req.query.endDate).subtract(9, 'hours'),
+            ],
+          },
+        },
+      })
+        .then((sales) => {
+          res.json(sales);
+        })
+        .catch((err) => {
+          console.error(err);
+          next(err);
+        });
+    }
+  })
+  .post(function (req, res) {
+    // 쓰기
+    Sales.create({
+      fitness_no: req.body.fitness_no,
+      client_name: req.body.client_name,
+      exerciseName: req.body.exerciseName,
+      exercisePrice: req.body.exercisePrice,
+      //locker:req.body.locker,
+      lockerPrice: req.body.lockerPrice,
+      //sportswear:req.body.sportswear,
+      sportswearPrice: req.body.sportswearPrice,
+      paymentTools: req.body.paymentTools,
+      paymentDate: req.body.paymentDate,
+      paidMembership: req.body.paidMembership,
+      salesStart_date: req.body.salesStart_date,
+      salesDays: req.body.salesDays,
+    })
+      .then(() => {
+        res.send({ success: 'sales update!' });
+      })
+      .catch((err) => {
         console.error(err);
-        next(err);
-        });*/
-    });
+      });
+  })
+  .put(function (req, res) {
+    // 수정
+  })
+  .delete(function (req, res) {
+    //삭제
+  });
 
 module.exports = router;
--- models/sales.js
+++ models/sales.js
@@ -1,20 +1,36 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('sales', {
-        num: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-        fitness_no: { type: DataTypes.INTEGER, allowNull: false },
-        member_no: { type: DataTypes.INTEGER, allowNull: false },
-        exerciseName: { type: DataTypes.STRING(32), allowNull: false },
-        exercisePrice: { type: DataTypes.INTEGER, allowNull: false },
-        //locker : {type:DataTypes.INTEGER},
-        lockerPrice: { type: DataTypes.INTEGER },
-        //sportswear : {type:DataTypes.INTEGER},
-        sportswearPrice: { type: DataTypes.INTEGER },
-        paymentTools: { type: DataTypes.STRING(32), allowNull: false },
-        paymentDate: { type: DataTypes.DATE, allowNull: false },
-        paidMembership: { type: DataTypes.INTEGER, allowNull: false, defaultValue: DataTypes.NULL },
-        salesStart_date: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NULL },
-        salesDays: { type: DataTypes.STRING(128), allowNull: false, defaultValue: DataTypes.NULL }
-    }, {
-        timestamps: false
-    });
-}
+  return sequelize.define(
+    'sales',
+    {
+      num: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      fitness_no: { type: DataTypes.INTEGER, allowNull: false },
+      client_name: { type: DataTypes.STRING(20), allowNull: false },
+      exerciseName: { type: DataTypes.STRING(32), allowNull: false },
+      exercisePrice: { type: DataTypes.INTEGER, allowNull: false },
+      //locker : {type:DataTypes.INTEGER},
+      lockerPrice: { type: DataTypes.INTEGER },
+      //sportswear : {type:DataTypes.INTEGER},
+      sportswearPrice: { type: DataTypes.INTEGER },
+      paymentTools: { type: DataTypes.STRING(32), allowNull: false },
+      paymentDate: { type: DataTypes.DATE, allowNull: false },
+      paidMembership: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: DataTypes.NULL,
+      },
+      salesStart_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NULL,
+      },
+      salesDays: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        defaultValue: DataTypes.NULL,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
