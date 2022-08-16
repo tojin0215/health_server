ff555842cb38ca4451689701a0c3f20ddbe7f6bb
xcv <xcv4716@naver.com>
2022-08-16 11:03:47 +0900

    asd

--- api/voucher.js
+++ api/voucher.js
@@ -6,7 +6,22 @@ const sequelize = require('sequelize');
 
 router
   .route('/voucher')
-  .get(function (req, res) {})
+  .get(function (req, res) {
+    Voucher.findAll({
+      where: {
+        fitness_no: req.query.fitness_no,
+        client_name: req.query.client_name,
+      },
+    })
+      .then((voucher) => {
+        res.json(voucher);
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      });
+  })
   .post(function (req, res) {})
   .put(function (req, res) {})
   .delete(function (req, res) {});
+module.exports = router;
