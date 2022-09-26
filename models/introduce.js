dac711d387bd380f32733e6544d9b819d71f2a12
seungwoo <seungwoo3859@gmail.com>
2022-09-26 11:44:05 +0900

    rollback

--- models/introduce.js
+++ models/introduce.js
@@ -9,7 +9,6 @@ module.exports = (sequelize, DataTypes) => {
         autoIncrement: true,
       },
       fitness_no: { type: DataTypes.INTEGER, allowNull: false },
-      post_title: { type: DataTypes.STRING(40), allowNull: false },
       manager_name: { type: DataTypes.STRING(20), allowNull: false },
       picture: { type: DataTypes.STRING(100), allowNull: true },
       story: { type: DataTypes.STRING(10000), allowNull: false },
